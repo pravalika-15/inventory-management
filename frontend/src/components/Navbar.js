4fa655d3781d57fd1e9a134ff739d71c8e8ae7d9
pravalika attada <pravalikaattada15@gmail.com>
2023-08-16 14:45:37 +0530

    99%

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -29,7 +29,7 @@ const NavBar = ({ authenticated, onLogout, userData }) => {
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://main--sensational-muffin-3ff308.netlify.app"
+            href="https://sensational-muffin-3ff308.netlify.app/"
             className="flex items-center"
           >
             <img
